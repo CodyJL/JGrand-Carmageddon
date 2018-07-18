33ecd45b638045905de3d2445fab95ae97b58e40
CodyJ <codylewiz@gmail.com>
2018-07-18 11:01:05 -0400

    Commit

---
+++ [GC]/GCMini/Settings.lua
@@ -0,0 +1,6 @@
+radarSettings = {
+	['mapTexture'] = 'files/images/map.jpg', -- Don't change
+	['mapTextureSize'] = 3072, -- Don't change
+	['mapWaterColor'] = {198, 222, 222}, -- Adjusted for this map
+	['alpha'] = 240, -- Can change
+};
